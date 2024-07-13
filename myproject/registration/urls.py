980e3e4e4bb3f658a43f35c73d75468f9b3c76f9
Teddy Siaka <99632830+Siaka385@users.noreply.github.com>
2024-07-13 14:26:25 +0300

    Add files via upload
    INTIAL COMIT

---
+++ myproject/registration/urls.py
@@ -0,0 +1,10 @@
+# accounts/urls.py
+
+from django.urls import path
+from .views import register, user_login, user_logout
+
+urlpatterns = [
+    path('', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+]
